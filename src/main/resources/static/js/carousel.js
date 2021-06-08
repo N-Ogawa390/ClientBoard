74c5fe16cdbe5699050b06ed47934080acf763f8
Nao <underneathnao@hotmail.co.jp>
2021-06-08 15:16:06 +0900

    3Dcarousel changed

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -1,19 +1,27 @@
 /**
  * 3Dカルーセル
  */
-window.onload = function(){
+window.addEventListener('load', () => {
 	
-	var carousel = document.querySelector('.carousel3d'),
-		figure = carousel.querySelector('figure'),
-		nav = carousel.querySelector('nav'),
-		numImages = figure.childElementCount,
-		curImage = 0,
-		theta = 360 / numImages,
-		img = figure.children[0],
-		imgWidth = 400,
-//		imgWidth = parseFloat(getComputedStyle(img).width),
-		disCenter = (imgWidth / 2) * Math.tan((90 - (180 / numImages)) * (Math.PI / 180));	//画像から中心に垂線を引いた距離
-		
+	var carousel3d = document.querySelector('.carousel3d');
+	carousel(carousel3d);
+});
+
+function carousel(root) {
+	
+	var
+	figure = root.querySelector('figure'),
+	images = figure.children,
+	numImages = images.length,
+	curImage = 0;
+	theta = 360 / numImages,
+	img = figure.children[0],
+	imgWidth = parseFloat(getComputedStyle(img).width),
+//	imgWidth = parseFloat(getComputedStyle(img).width) * 1.25,
+	disCenter = (imgWidth / 2) * Math.tan((90 - (180 / numImages)) * (Math.PI / 180)),	//画像から中心に垂線を引いた距離
+	nav = root.querySelector('nav');
+	console.log(imgWidth);
+	
 	figure.style.transformOrigin = `center center -${disCenter}px`;
 	for (i=1; i < numImages; i++) {
 		figure.children[i].style.transform = `rotateY(${i * theta}deg)`;
@@ -22,20 +30,6 @@ window.onload = function(){
 	
 	//カルーセル回転
 	nav.addEventListener('click', navCarousel);
-
-	function navCarousel(e) {
-		var t = e.target;
-		if (t.tagName.toUpperCase() != 'BUTTON'){
-			return;
-		}
-		if (t.classList.contains('next')) {
-			curImage++;
-		}
-		else {
-			curImage--;
-		}
-		figure.style.transform = `rotateY(-${theta * curImage}deg)`;
-	}
 	
 	//imgクリック時のイベント(画像をポップアップ)を追加
 	var images = figure.querySelectorAll('img');
@@ -49,8 +43,25 @@ window.onload = function(){
 		});
 	});
 	
-	//画像にクリックイベントを付与してから表示
-	function dispImgFnc(dispImg){
+	//カルーセル回転
+	function navCarousel(e) {
+		
+		var t = e.target;
+		
+		if (t.tagName.toUpperCase() != 'BUTTON'){
+			return;
+		}
+		if (t.classList.contains('next')) {
+			curImage++;
+		}
+		else {
+			curImage--;
+		}
+		figure.style.transform = `rotateY(${theta * curImage * (-1)}deg)`;
+	}
+	
+	//画像にポップアップイベントを付与
+	function dispImgFnc(dispImg) {
 		dispImg.style.transform = null;
 		dispImg.classList.add('disp-img', 'show');
 		dispImg.addEventListener('click', function(e){
@@ -64,5 +75,73 @@ window.onload = function(){
 			});
 		});
 		b.prepend(dispImg);
-	}
-}
+	};
+};
+
+
+//window.onload = function(){
+//	
+//	var carousel = document.querySelector('.carousel3d'),
+//	figure = carousel.querySelector('figure'),
+//	nav = carousel.querySelector('nav'),
+//	numImages = figure.childElementCount,
+//	curImage = 0,
+//	theta = 360 / numImages,
+//	img = figure.children[0],
+//	imgWidth = window.outerWidth,
+////		imgWidth = 400,
+////		imgWidth = parseFloat(getComputedStyle(img).width),
+//	disCenter = (imgWidth / 2) * Math.tan((90 - (180 / numImages)) * (Math.PI / 180));	//画像から中心に垂線を引いた距離
+//	
+//	figure.style.transformOrigin = `center center -${disCenter}px`;
+//	for (i=1; i < numImages; i++) {
+//		figure.children[i].style.transform = `rotateY(${i * theta}deg)`;
+//		figure.children[i].style.transformOrigin = `center center -${disCenter}px`;
+//	}
+//	
+//	//カルーセル回転
+//	nav.addEventListener('click', navCarousel);
+//	
+//	function navCarousel(e) {
+//		var t = e.target;
+//		if (t.tagName.toUpperCase() != 'BUTTON'){
+//			return;
+//		}
+//		if (t.classList.contains('next')) {
+//			curImage++;
+//		}
+//		else {
+//			curImage--;
+//		}
+//		figure.style.transform = `rotateY(-${theta * curImage}deg)`;
+//	}
+//	
+//	//imgクリック時のイベント(画像をポップアップ)を追加
+//	var images = figure.querySelectorAll('img');
+//	var b = document.querySelector('body');
+//	
+//	images.forEach(function(i){
+//		i.addEventListener('click', function(e){
+//			var dispImg = e.target.cloneNode();
+//			
+//			dispImgFnc(dispImg);
+//		});
+//	});
+//	
+//	//画像にクリックイベントを付与してから表示
+//	function dispImgFnc(dispImg){
+//		dispImg.style.transform = null;
+//		dispImg.classList.add('disp-img', 'show');
+//		dispImg.addEventListener('click', function(e){
+//			var t = e.target;
+////			var bs = document.getElementsByClassName('block-screen').item(0);
+//			t.classList.remove('show');
+//			t.classList.add('display-none');
+//			t.addEventListener('animationend', function(){
+//				t.parentNode.removeChild(t);
+////				bs.parentNode.removeChild(bs);
+//			});
+//		});
+//		b.prepend(dispImg);
+//	}
+//}
