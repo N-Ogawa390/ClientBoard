4e648e1a99088a0f31e197bd1ab499661e50d429
Nao <underneathnao@hotmail.co.jp>
2021-06-08 18:05:14 +0900

    error check

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -26,7 +26,7 @@ function carousel(root) {
 	
 	figure.style.transformOrigin = `center center -${disCenter}px`;
 	for (i=1; i < numImages; i++) {
-		figure.children[i].style.transformOrigin = 'center center -137px';
+		figure.children[i].style.transformOrigin = '50% 50% -137px';
 		figure.children[i].style.transform = `rotateY(${i * theta}deg)`;
 //		figure.children[i].style.transformOrigin = `center center -${disCenter}px`;
 //		figure.children[i].style.transform = `rotateY(${i * theta}deg)`;
