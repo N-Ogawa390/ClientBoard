6a67bcf9e530e4cdd02f88bef8dc45af8e5066e2
Nao <underneathnao@hotmail.co.jp>
2021-06-08 22:10:48 +0900

    3D carousel changed

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -29,7 +29,7 @@ function carousel(root) {
 	
 	for (i=1; i < numImages; i++) {
 //		figure.children[i].style.transformOrigin = 'center center -137px';
-//		figure.children[i].style.transformOrigin = `center center -${disCenter}px`;
+		figure.children[i].style.transformOrigin = `center center ${-disCenter}px`;
 		figure.children[i].style.transform = `rotateY(${i * theta}deg)`;
 	}
 	
