9bb4a2c17606b9c6cada8895fc1a72cee0cb37d3
naotoshi ogawa <boomboxxxs@gmail.com>
2022-02-04 14:55:29 +0900

    add schedule layout

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,35 @@
 /**
  * 
  */
+ 
+/* スケジュール＞アイテム：文字がコンテンツ幅に収まるようフォントサイズを自動調整する */
+
+window.addEventListener('DOMContentLoaded', () => {
+	if(document.getElementsByClassName('schedule-outline').length) {
+		const scheduleItems = document.getElementsByClassName('schedule-item');
+		let texts;
+		let range = new Range();
+		for(let i = 0; i < scheduleItems.length; i++) {
+			texts = scheduleItems[i].children;
+			for(let j = 0; j < texts.length; j++) {
+				range.selectNode(texts[j]);
+				if(range.getClientRects().length > 2) {
+//					console.log(texts[j].textContent);
+//					fitty(texts[j], {
+//						observeMutations: MutationObserverInit
+//					});
+					adjustTextForOneline(texts[j]);
+				}
+			}
+		}
+	}
+})
+
+function adjustTextForOneline (textNode) {
+//	let size = window.getComputedStyle(textNode).fontSize.split('px')[0];
+//	size = size - 12;
+//	textNode.style.zoom = '0.5';
+}
 
 /* 画像フェードイン */
 $(function(){
